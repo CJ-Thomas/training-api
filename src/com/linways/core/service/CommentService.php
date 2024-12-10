4649442569b1f5c0e9b0290f8b0e900c0d221c13
Intern User <thomasjacob@INTERN-TEST-001.ccb2culnqikuviq4xwkfpgv3ce.rx.internal.cloudapp.net>
2024-12-10 12:58:47 +0530

    ADDED: comment services and like services

--- src/com/linways/core/service/CommentService.php
+++ src/com/linways/core/service/CommentService.php
@@ -1,58 +1,113 @@
 <?php
-    namespace com\linways\core\service;
-    use com\linways\base\util\MakeSingletonTrait;
-    use com\linways\core\dto\Comment;
 
-    class CommentService extends BaseService{
-        use MakeSingletonTrait;
+namespace com\linways\core\service;
 
-        /**
-         * @param Comment $comment
-         * @return Comment $comment
-         */
-        public function createComment(Comment $comment){
-            $comment = $this-> realEscapeObject($comment);
+use com\linways\base\util\MakeSingletonTrait;
+use com\linways\core\dto\Comment;
+use com\linways\core\util\UuidUtil;
+use Exception;
 
+class CommentService extends BaseService
+{
+    use MakeSingletonTrait;
 
-            $query = "INSERT INTO comments (id, user_id, post_id, comment, parent_comment_id, created_by, updated_by)
-            VALUES('$comment->id','$comment->userId','$comment->postId','$comment->comment','','','');";
-            return $comment;
-        }
-        
+    /**
+     * create a new comment
+     * @param Comment $comment
+     * @return Comment $comment
+     */
+    public function createComment(Comment $comment)
+    {
+        $comment = $this->realEscapeObject($comment);
+        $comment->createdBy = $GLOBALS["userId"] ?? $comment->createdBy;
+        $comment->updatedBy = $GLOBALS["userId"] ?? $comment->updatedBy;
 
-        /**
-         * @param Comment $comment
-         * @return Comment $comment
-         */
-        public function editComment(Comment $comment){
-            $comment = $this-> realEscapeObject($comment); 
-            
-            return $comment;
-        }
+        $comment->id = UuidUtil::guidv4();
 
-        /**
-         * @param Comment $comment
-         */
-        public function deleteComment(Comment $comment){
-            $comment = $this-> realEscapeObject($comment); 
+        $query = "INSERT INTO comments (id, user_id, post_id, comment, parent_comment_id, created_by, updated_by)
+            VALUES('$comment->id','$comment->userId','$comment->postId','$comment->comment','$comment->parentCommentId',
+            '$comment->createdBy','$comment->updatedBy');";
+
+        try {
+            $this->executeQuery($query);
+        } catch (Exception $e) {
+            throw new Exception("UNABLE TO INSERT INTO DB");
         }
 
-        /**
-         * @param string $postId
-         * @return Comments[]
-         */
-        public function getAllPostComments(string $postId){
-            $postId = $this-> realEscapeString($postId);
 
-        }
+        return $comment;
+    }
 
-        /**
-         * @param string $commentId
-         * @return Comments[]
-         */
-        public function getAllCommentReplies(string $commentId){
-            $commentId = $this-> realEscapeString($commentId);
+
+    /**
+     * edit an existing comment
+     * @param string $id
+     * @param string $comment
+     */
+    public function editComment(string $id, string $comment)
+    {
+        $id = $this->realEscapeString($id);
+        $comment = $this->realEscapeString($comment);
+
+        if(empty($comment))
+            return "";
+
+        $query = "UPDATE comments SET commet = '$comment' WHERE id = '$id';";
+
+        try {
+
+            $result = ($this->executeQuery($query))->sqlResult;
+
+        } catch (Exception $e) {
+            throw $e;
         }
 
     }
-?>
+
+    /**
+     * delete a comment
+     * @param string $id
+     * @return bool
+     */
+    public function deleteComment(string $id)
+    {
+        $id = $this->realEscapeString($id);
+
+        $query = "DELETE FROM comments WHERE id LIKE '$id';";
+
+        try {
+
+            $result = ($this->executeQuery($query))->sqlResult;
+
+        } catch (\Exception $e) {
+            throw $e;
+        }
+        return $result;
+    }   
+
+    /**
+     * Get all comments related to a post
+     * @param string $postId
+     * @return Comments[]
+     */
+    public function getAllPostComments(string $postId)
+    {
+        $postId = $this->realEscapeString($postId);
+        
+        $query = "SELECT id, user_id, comment, parent_comment_id FROM comments WHERE post_id = '$postId';";
+
+        try {
+
+            $result = ($this->executeQuery($query))->sqlResult;
+            while ($object = $result->fetch_object())
+            $comments[] = $object;
+
+
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        return $comments;
+    }
+
+}
--- src/com/linways/core/service/LikeService.php
+++ src/com/linways/core/service/LikeService.php
@@ -2,6 +2,8 @@
 namespace com\linways\core\service;
 use com\linways\base\util\MakeSingletonTrait;
 use com\linways\core\dto\Like;
+use com\linways\core\util\UuidUtil;
+use Exception;
 
 class LikeService extends BaseService{
 
@@ -11,7 +13,19 @@ class LikeService extends BaseService{
      */
     public function createLike(Like $like){
         $like = $this-> realEscapeObject($like);
-        
+        $like->createdBy = $GLOBALS["userId"] ?? $like->createdBy;
+        $like->updatedBy = $GLOBALS["userId"] ?? $like->updatedBy;
+
+        $like->id = UuidUtil::guidv4();
+
+        $query = "INSERT INTO likes (id, user_id, post_id, created_by, updated_by)
+        VALUES('$like->id', '$like->userId', '$like->postId', '$like->createdBy',
+        '$like->updatedBy');";
+        try {
+            $this->executeQuery($query);
+        } catch (Exception $e) {
+            throw new Exception("UNABLE TO INSERT INTO DB");
+        }
     }
     
     /**
@@ -19,6 +33,14 @@ class LikeService extends BaseService{
      */
     public function removeLike(Like $like){
         $like = $this-> realEscapeObject($like);
+
+        $query = "DELETE FROM likes WHERE id LIKE '$like->id';";
+
+        try {
+            $this->executeQuery($query);
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
 
 
@@ -26,8 +48,16 @@ class LikeService extends BaseService{
      * @param String $postId
      * @return Likes[]
      */
-    public function getTotalLikes(String $postId){
+    public function getTotaPostlLikes(String $postId){
         $postId = $this-> realEscapeString($postId);
+
+        $query = "SELECT COUNT(*) FROM likes WHERE post_id LIKE '$postId';";
+
+        try {
+            $result = ($this->executeQuery($query))->sqlResult;
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
 }
 ?>
